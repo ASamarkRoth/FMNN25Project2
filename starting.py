ba9abded135726b9452d6048504c698b6869fb7f
SamuelWiqvist <swiqvist@gmail.com>
2015-10-02 16:40:56 +0200

    test

--- starting.py
+++ starting.py
@@ -1,25 +1,10 @@
-
-
+"""
+@author: Anton Roth, Linus Jangland and Samuel Wiqvist 
+"""
 import numpy as np
 import scipy.linalg as sl
 import abc #abstract base classes
-
-'''Suggestion of how to compute grid for computation of g, perhaps also H?
-Options: Create grid from the start which we use throughout whole optimisation or 
-everytime we compute gk and Hk we compute new grid? Isn't it necessary to introduce a new grid everytime to assure
-that xk+1 exists in the grid? '''
-
-xk = np.array([3,4])
-step =  0.001
-bounds = [((xk[j]-step*(nbr_points//2)),xk[j]+1.1*step*(nbr_points//2)) for j in range(len(xk))]
-gridk = np.mgrid[[slice(bounds[j][0], bounds[j][1], step) for j in range(len(xk))]]
-
-f = gridk[0]** + gridk[1]*2
-g,dx = np.gradient(f)
-
-#%% 
-'''Design suggestion'''
-
+ 
 class OptimizationProblem:
     '''A class which generates the necessary components to handle and solve an
     optimization problem defined by an input function f'''
@@ -34,48 +19,59 @@ class OptimizationProblem:
     
     @staticmethod
     def guess_x(f):
-        return 1 #Can we do this?
+        return 1 #Can we do this? # dont we need to return a n-dim vector? 
         
-    @classmethod
-    def get_gradient(f,x0):
-        return 2
+    @classmethod 
+    def get_gradient(cls, f,x0):
+        return 2 # shoulden we compute the gradient numericaly now? 
     
 '''This class is intended to be inherited'''
 class OptimizationMethods:
     __metaclass__ = abc.ABCMeta
     
-    def __init__(self, OptimizationProblem):
+    def __init__(self, OptimizationProblem, par_line = "exact"):
         self.f = OptimizationProblem.f
         self.x0 = OptimizationProblem.x0
         self.g = OptimizationProblem.g
+        self.par_line = par_line 
     
     def newton_procedure():
         xk = self.x0.copy()
         fk = self.f.copy()
         gk = self.g.copy()
+        Gk = _initial_hessian(gk, xk)
         while True:
-            gk = OptimizationProblem.get_gradient(fk,x0)
-            Hk = self.hessian(gk,fk,x0)
-            sk = np.dot(inv(Hk),gk)
-            #Perhaps the two rows above should be one method, newton_direction
-            alphak = self.line_search(fk, xk, tol = 1e-5)
-            self.xk = self.xk + alphak
-            if sl.norm(np.dot(alphak,sk)) < self.tol:
+            sk = _newton_direction(Gk, gk)   
+            if par_line == "exact": 
+                alphak = self._line_search(fk, xk, tol = 1e-5, "exact")
+            elif par_line == "inexact":
+                alphak = self._line_search(fk, xk, tol = 1e-5, "inexact")
+            else:
+                alphak = 1
+            xk = xk + alphak*sk
+            Gk = _update_hessian(Gk, self.xk)
+            if sl.norm(alphak*sk) < self.tol:
                 x = xk
                 fmin = f(xk)
                 break
-        return x, fmin
-    
-    @abc.abstractmethod
-    def hessian():
-        '''computes and returns hessian or hessian approx.'''
+        return [x, fmin]
+        
+    def _newton_direction(Gk, gk):
+        '''Computes sk'''
+        return (-1)*np.np.linalg.solve(Gk, gk)
         
     @abc.abstractmethod
-    def newton_direction():
-        '''Computes newton_direction'''
+    def _initial_hessian():
+        '''Returns the inital hessian'''
     
     @abc.abstractmethod
-    def line_search():
+    def _update_hessian(): # is this also the update method 
+        '''Computes and returns hessian or hessian approx. 
+            Updates the hessian
+        '''
+    
+    @abc.abstractmethod
+    def _line_search():
         '''Performs a line_search, finds the alpha which minimizes f(xk+alpha*sk)'''
 
 
@@ -96,3 +92,16 @@ class Newton2(Newton):
     
     def get_new_x(self,k):
         return self.x0+k
+#%%
+'''Suggestion of how to compute grid for computation of g, perhaps also H?
+Options: Create grid from the start which we use throughout whole optimisation or 
+everytime we compute gk and Hk we compute new grid? Isn't it necessary to introduce a new grid everytime to assure
+that xk+1 exists in the grid? '''
+
+xk = np.array([3,4])
+step =  0.001
+bounds = [((xk[j]-step*(nbr_points//2)),xk[j]+1.1*step*(nbr_points//2)) for j in range(len(xk))]
+gridk = np.mgrid[[slice(bounds[j][0], bounds[j][1], step) for j in range(len(xk))]]
+
+f = gridk[0]** + gridk[1]*2
+g,dx = np.gradient(f)

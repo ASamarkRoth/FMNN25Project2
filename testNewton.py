efd8a0a8b895e51c81e92d6eb308e883a2dc0e14
Anton <ajaj@hotmail.com>
2015-10-05 17:51:50 +0200

    Något som jag inte kommer ihåg

---
+++ testNewton.py
@@ -0,0 +1,37 @@
+import unittest
+import numpy as np
+from optimizationNewton import *
+import linesearch
+from rosenbrock import *
+
+class TestEverything(unittest.TestCase):
+    def setUp(self):
+        function = rosenbrock
+        self.guess = [37,-34]
+        self.problem = OptimizationProblem(function, x0 = self.guess)
+  
+    def test_exact_line_search(self):
+        f = lambda x: (x-8)**6
+        alpha = linesearch.exact_line_search(f)
+        self.assertAlmostEqual(alpha,8)
+    
+    def test_inexact_line_search(self):
+        f = lambda x: (x-8)**6
+        fp = lambda x: 6*(x-8)**5
+        alpha = linesearch.inexact_line_search(f, fp, 1)
+        self.assertAlmostEqual(alpha,8)
+
+    def test_original_newton(self):
+        solve = OriginalNewton(self.problem)
+        xstar, fmin = solve.newton_procedure(par_line_search = "exact")
+        self.assertAlmostEqual(xstar.all(),1 , 4)
+        self.assertAlmostEqual(fmin , 0, 4)
+        xstar, fmin = solve.newton_procedure(par_line_search = "inexact")
+        self.assertAlmostEqual(xstar.all(),1 , 4)
+        self.assertAlmostEqual(fmin , 0, 4)
+    
+        
+if __name__ == '__main__':
+	unittest.main()
+
+'''def test_exception(self):self.assertRaises(ValueError, self.s, self.grid[1])'''
